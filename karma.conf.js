6ff707c5af805076fdd0ef94eb3ea178440a80a3
kumar <scplconstructiongroup@gmail.com>
2023-06-10 23:38:41 +0530

    projectlevelchanges

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/personal-portfoli-angular'),
+      dir: require('path').join(__dirname, './coverage/scplgroup'),
       subdir: '.',
       reporters: [
         { type: 'html' },
