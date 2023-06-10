e3b1aac54d6ac359967a8e8266c5c54ec68c835d
kumar <scplconstructiongroup@gmail.com>
2023-06-11 00:16:48 +0530

    imagechanges

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -16,168 +16,168 @@ export class ProfileService {
   projects:any = [
     {
       id: 1,
-      title: 'Read data from Google spreadsheet in Angular',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/MyShop/#/',
       githurl: 'https://github.com/mehulk05/MyShop',
       mediumlink: 'https://itnext.io/read-data-from-google-spreadsheet-in-angular-209ee74b6cb4',
-      imgUrl: 'assets/images/z1.png',
-      tech: 'Angular 10 , Google SpreadsheetApi'
+      imgUrl: 'assets/images/z1.jpg',
+      tech: 'Site Description Placeholder'
     },
 
     {
       id: 2,
-      title: 'Blog App Using MERN Stack',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/Blogapp-using-MERN/#/',
       githurl: 'https://github.com/mehulk05/Blogapp-using-MERN',
       mediumlink: 'https://itnext.io/blog-app-using-mern-stack-b0b4d69d7ea1',
-      imgUrl: 'assets/images/z26.png',
-      tech: 'React, Node, MongoDB, Express'
+      imgUrl: 'assets/images/z2.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
     {
       id: 3,
-      title: 'CRUD operation using MEAN stack',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/Blog-using-mean/#/',
       githurl: 'https://github.com/mehulk05/Blog-using-mean',
       mediumlink: 'https://itnext.io/crud-operation-using-mean-stack-7dfa2f51ec8c',
-      imgUrl: 'assets/images/z27.png',
-      tech: 'Angular 10, Node, MongoDB, Express'
+      imgUrl: 'assets/images/z3.jpg',
+      tech: 'Site Description Placeholder'
     },
 
     {
       id: 4,
-      title: 'Multiroom Chat Application in Node JS',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://chatapp-using-node.herokuapp.com/',
       githurl: 'https://github.com/mehulk05/Chat-app-using-Nodejs',
       mediumlink: 'https://medium.com/weekly-webtips/building-a-multiroom-chat-application-in-node-js-8a8adca5acf2',
-      imgUrl: 'assets/images/z23.png',
-      tech: 'Node JS,Socket IO'
+      imgUrl: 'assets/images/z4.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
     {
       id: 5,
-      title: 'Integrate Medium Blogs on your React App',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/Medium-Blog-On-React-App#/',
       githurl: 'https://github.com/mehulk05/Medium-Blog-On-React-App',
       mediumlink: 'https://javascript.plainenglish.io/how-to-add-medium-blogs-on-your-react-app-a3898a729aad',
-      imgUrl: 'assets/images/z4.png',
-      tech: 'React JS'
+      imgUrl: 'assets/images/z5.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
 
     {
       id: 6,
-      title: 'News App using ReactJS (Parsing XML to JSON)',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/NewsApp-using-React',
       githurl: 'https://github.com/mehulk05/NewsApp-using-React',
       mediumlink: 'https://codeburst.io/news-app-using-reactjs-f9752d7ab6ce',
-      imgUrl: 'assets/images/z24.png',
-      tech: 'React JS, XMl to JSON'
+      imgUrl: 'assets/images/z6.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
 
     {
       id: 7,
-      title: 'Medium-Like Blog App Using Angular 9 and Firebase',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/Blog-Book/',
       githurl: 'https://github.com/mehulk05/Blog-Book',
       mediumlink: 'https://codeburst.io/blogging-app-using-angular-9-and-firebase-98e36168d850',
-      imgUrl: 'assets/images/z21.png',
-      tech: 'HTML, Bootstrap, CSS, Angular-9, Firebase'
+      imgUrl: 'assets/images/z7.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
 
     {
       id: 8,
-      title: 'COVID 19 Tracker (Statistics) app Using Angularr',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/COVOID-19/',
       githurl: 'https://github.com/mehulk05/COVOID-19',
       mediumlink: 'https://codeburst.io/covoid-19-tracker-statistics-app-using-angular-2358347b3d1f',
-      imgUrl: 'assets/images/z2.png',
-      tech: 'Angular 10 ,Bootstrap,Javascript'
+      imgUrl: 'assets/images/z8.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
     {
       id: 9,
-      title: 'Weather App with React',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulk05.github.io/React-Weather/',
       githurl: 'https://github.com/mehulk05/React-Weather-App',
       mediumlink: 'https://medium.com/analytics-vidhya/weather-app-with-react-adaa7268bbdb',
-      imgUrl: 'assets/images/z9.png',
-      tech: 'React, Open MAp Api'
+      imgUrl: 'assets/images/z9.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
     {
       id: 10,
-      title: 'Spring Boot + Angular 8 CRUD Example',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
       githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
       mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
-      imgUrl: 'assets/images/z28.png',
-      tech: 'Angular8, Java ,Hibernate'
+      imgUrl: 'assets/images/z10.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
     {
       id: 11,
-      title: 'Personal Portfolio Using Angular',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
       githurl: 'https://github.com/mehulk05/Personal-Portfolio-Angular-Updated',
       mediumlink: 'https://javascript.plainenglish.io/personal-portfolio-using-angular-8-and-bootstrap-5dde6fc7cf9b',
-      imgUrl: 'assets/images/z11.png',
-      tech: 'Angular ,Bootstrap'
+      imgUrl: 'assets/images/z11.jpg',
+      tech: 'Site Description Placeholder'
     },
 
 
     {
       id: 12,
-      title: 'Personal Portfolio Using Django',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
       githurl: 'https://github.com/mehulk05/My-Personal-Portfolio',
       mediumlink: 'https://mehulkothari05.medium.com/personal-portfolio-using-django-dba49c355905',
-      imgUrl: 'assets/images/z12.png',
-      tech: 'Django'
+      imgUrl: 'assets/images/z12.jpg',
+      tech: 'Site Description Placeholder'
     },
 
     {
       id: 13,
-      title: 'Movie Success Prediction',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulkothari05.medium.com/',
       githurl: 'https://github.com/mehulk05',
       mediumlink: 'https://mehulkothari05.medium.com/movie-success-prediction-a67d871a7b1',
-      imgUrl: 'assets/images/z13.png',
-      tech: 'Python, Machine Learning, Django Framework.'
+      imgUrl: 'assets/images/z13.jpg',
+      tech: 'Site Description Placeholder'
     },
 
     {
       id: 14,
-      title: 'Matrimonial Website',
+      title: 'Site Pictures Placeholder',
       desc: '',
       livedemo: 'https://mehulkothari05.medium.com/',
       githurl: 'https://github.com/mehulk05/Matrimonial-Website.git',
       mediumlink: 'https://mehulkothari05.medium.com/matrimonial-website-9cd5247f477d',
-      imgUrl: 'assets/images/z14.png',
-      tech: 'HTML, CSS, Bootstrap, Jquery'
+      imgUrl: 'assets/images/z14.jpg',
+      tech: 'Site Description Placeholder'
     },
 
   ]
